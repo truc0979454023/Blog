e72195e8456aacb2416bcf6a68446f5f5a850581
minhtruc <truc0979454023@gmail.com>
2024-06-07 23:50:29 +0700

    lession2.6

---
+++ src/Blog.Api/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
--- src/Blog.Api/Program.cs
+++ src/Blog.Api/Program.cs
@@ -1,6 +1,60 @@
+using Blog.Core.Domain.Identity;
+using Blog.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+//Config DB Context and ASP.net core identity
+builder.Services.AddDbContext<BlogContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<BlogContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //Password settings
+    options.Password.RequireDigit = true; // s?
+    options.Password.RequireLowercase = true; //th??ng
+    options.Password.RequireNonAlphanumeric = true; //ký t? ??c bi?c
+    options.Password.RequireUppercase = true; //hoa
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    //Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);//sau bao lâu s? t? logout
+    options.Lockout.MaxFailedAccessAttempts = 5; //S? l?n login fail b? khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    //User settings
+    options.User.AllowedUserNameCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+    options.User.RequireUniqueEmail = false;
+});
+
+
+/*builder.Services.AddScoped(typeof(IReadOnlyDictionary<,>), typeof(RepositotyBase<,>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//Business services and repositories
+var services = typeof(PostRepository).Assembly.GetTypes()
+    .Where(x => x.GetInterfaces().Any(i => i.Name == typeof(IRepository<,>).Name)
+    && !x.IsAbstract && x.IsClass && !x.IsGenericType);
+
+foreach (var service in services)
+{
+    var allInterfaces = service.GetInterfaces();
+    var direcInterface = allInterfaces.Except(allInterfaces.SelectMany(t => t.GetInterfaces())).FirstOrDefault();
+    if (direcInterface != null)
+    {
+        builder.Services.Add(new ServiceDescriptor(direcInterface, service, ServiceLifetime.Scoped));
+    }
+}*/
+
+
+//Default config to the container
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
